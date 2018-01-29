77e5106f8777ae843a2f346b5a49374afa8124ad
Christian Ng <christianr.ng@gmail.com>
2018-01-28 23:14:42 -0800

    Burp's exposed functions are slower than python libs

--- entropy.py
+++ entropy.py
@@ -1,3 +1,5 @@
+BASE64_CHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/="
+HEX_CHARS = "1234567890abcdefABCDEF"
 def shannon_entropy(data, iterator):
     """
     Borrowed from http://blog.dkbza.org/2007/05/scanning-data-for-entropy-anomalies.html
@@ -10,3 +12,37 @@ def shannon_entropy(data, iterator):
         if p_x > 0:
             entropy += - p_x*math.log(p_x, 2)
     return entropy
+
+def find_entropy(text):
+    stringsFound = []
+    lines = text.split("\n")
+    for line in lines:
+        for word in line.split():
+            base64_strings = get_strings_of_set(word, BASE64_CHARS)
+            hex_strings = get_strings_of_set(word, HEX_CHARS)
+            for string in base64_strings:
+                b64Entropy = shannon_entropy(string, BASE64_CHARS)
+                if b64Entropy > 4.5:
+                    stringsFound.append(string)
+            for string in hex_strings:
+                hexEntropy = shannon_entropy(string, HEX_CHARS)
+                if hexEntropy > 3:
+                    stringsFound.append(string)
+    return stringsFound
+
+def get_strings_of_set(word, char_set, threshold=20):
+    count = 0
+    letters = ""
+    strings = []
+    for char in word:
+        if char in char_set:
+            letters += char
+            count += 1
+        else:
+            if count > threshold:
+                strings.append(letters)
+            letters = ""
+            count = 0
+    if count > threshold:
+        strings.append(letters)
+    return strings
--- json_grab.py
+++ json_grab.py
@@ -1,5 +1,36 @@
 from burp import IBurpExtender
+from 
 class BurpExtender(IBurpExtender):
     def registerExtenderCallbacks( self, callbacks):
         # your extension code here
-        return
+         self._callbacks = callbacks
+        
+        # set our extension name
+        callbacks.setExtensionName("JSON Grab")
+        
+        # obtain our output stream
+        self._stdout = PrintWriter(callbacks.getStdout(), True)
+
+        # register ourselves as an HTTP listener
+        callbacks.registerHttpListener(self)
+        
+        # register ourselves as a Proxy listener
+        callbacks.registerProxyListener(self)
+    #
+    # implement IHttpListener
+    #
+
+    def processHttpMessage(self, toolFlag, messageIsRequest, messageInfo):
+        self._stdout.println(
+                ("HTTP request to " if messageIsRequest else "HTTP response from ") +
+                messageInfo.getHttpService().toString() +
+                " [" + self._callbacks.getToolName(toolFlag) + "]")
+
+    #
+    # implement IProxyListener
+    #
+
+    def processProxyMessage(self, messageIsRequest, message):
+        self._stdout.println(
+                ("Proxy request to " if messageIsRequest else "Proxy response from ") +
+                message.getMessageInfo().getHttpService().toString())
